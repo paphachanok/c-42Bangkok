0f8c1a5095987f7d5cb3102935890f9db20012e3
paphachanok <108296059+paphachanok@users.noreply.github.com>
2022-07-19 09:29:39 +0700

    Add files via upload

---
+++ c_02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int ft_strlen(char *str)
+{
+    int j;
+
+    j = 0;
+    while (str[j] != '\0')
+    {
+        j++;
+        str++;
+    }
+    return (j);
+}
+
+int ft_str_is_lowercase(char *str)
+{
+    int i;
+
+    i = 0;
+    if(ft_strlen(str) != 0)
+    {
+        while (str[i] != '\0')
+        {
+            if (str[i] >= 'a' && str[i] <= 'z')
+                i++;
+            else
+                return (0);
+        }
+        return (1);
+    }
+    else
+    {
+        return (1);
+    }
+}
+
+int main(void)
+{
+    char str[10] = "asdfghj";
+    char str2[10] = "asdfHghj";
+    char str3[10] = "1234a";
+    char str4[10];
+
+    printf("%d\n", ft_str_is_lowercase(str));
+    printf("%d\n", ft_str_is_lowercase(str2));
+    printf("%d\n", ft_str_is_lowercase(str3));
+    printf("%d", ft_str_is_lowercase(str4));
+}
---
+++ c_02/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	j;
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		j++;
+		str++;
+	}
+	return (j);
+}
+
+int	ft_str_is_uppercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (ft_strlen(str) != 0)
+	{
+		while (str[i] != '\0')
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				i++;
+			else
+				return (0);
+		}
+		return (1);
+	}
+	else
+		return (1);
+}
+
+/*int	main(void)
+{
+	char	str[10] = "ASDFGH";
+	char	str2[10] = "aaaaHg";
+	char	str3[10] = "12345H";
+	char	str4[10];
+
+	printf("%d\n", ft_str_is_uppercase(str));
+	printf("%d\n", ft_str_is_uppercase(str2));
+	printf("%d\n", ft_str_is_uppercase(str3));
+	printf("%d", ft_str_is_uppercase(str4));
+}*/
