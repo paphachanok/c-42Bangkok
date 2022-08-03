f072dc9f99718c6f048f11978349b320430ca26e
paphachanok <108296059+paphachanok@users.noreply.github.com>
2022-08-03 21:42:29 +0700

    Add files via upload

--- final_final/ft_atoi_base.c
+++ final_final/ft_atoi_base.c
@@ -1,18 +1,40 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ppoti <marvin@42.fr>                       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/03 15:44:39 by ppoti             #+#    #+#             */
-/*   Updated: 2022/08/03 16:30:16 by ppoti            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
 
 int	ft_atoi_base(const char *str, int str_base)
 {
+	int	i;
+	int	n;
+	int	result;
 
+	i = 0;
+	n = 1;
+	result = 0;
+	if (str[i] == '-')
+	{
+		n = -1;
+		i++;
+	}
+	while (str[i] != '\0')
+	{
+		result = result * str_base;
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			result = result + (str[i] - 48);
+		}
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			result = result + (str[i] - 55);
+		}
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			result = result + (str[i] - 87);
+		}
+		i++;
+	}
+	return (result);
+}
+
+int	main(void)
+{
+	printf("%d", ft_atoi_base("3c", 16));
 }
